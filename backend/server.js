d306c98ad2760d2ad0e024b2aea1c8a8b1ce13ca
LOVESH1808 <loveshdutta1808@gmail.com>
2024-05-26 07:00:16 +0530

    testing6

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use("/api/message", messageRoutes)
 // ------------------DEPLOYMENT----------------
 
 const __dirname1 = path.resolve()
-if(process.env.NODE_ENV === 'production') {
+if(process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'development') {
     app.use(express.static(path.join(__dirname1, "/frontend/build")))
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
